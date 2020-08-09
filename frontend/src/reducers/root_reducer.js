5706d82b517aa9128d30295e83aece67bf7d6840
omelnykov1 <sashqa.melnikov@gmail.com>
2020-08-09 14:01:07 -0400

    added modal

--- frontend/src/reducers/root_reducer.js
+++ frontend/src/reducers/root_reducer.js
@@ -1,10 +1,14 @@
 import { combineReducers } from "redux";
 import session from "./session_reducer";
-import errors from "./errors_reducer"
+import errors from "./errors_reducer";
+import modal from "./modal_reducer";
+import entities from "./entities_reducer"
 
 const RootReducer = combineReducers({
   session,
-  errors
+  errors,
+  modal,
+  entities,
 });
 
 export default RootReducer;
--- frontend/src/util/route_util.js
+++ frontend/src/util/route_util.js
@@ -1,38 +1,32 @@
-import React from "react";
+import { Route, withRouter, Redirect } from "react-router-dom";
 import { connect } from "react-redux";
-import { Route, Redirect, withRouter } from "react-router-dom";
+import React from "react";
+import { openModal } from "../actions/modal_actions";
 
 const Auth = ({ component: Component, path, loggedIn, exact }) => (
   <Route
     path={path}
     exact={exact}
     render={(props) =>
-      !loggedIn ? (
-        <Component {...props} />
-      ) : (
-        <Redirect to="/tweets" />
-      )
+      !loggedIn ? <Component {...props} /> : <Redirect to="/" />
     }
   />
 );
 
-const Protected = ({ component: Component, loggedIn, ...rest }) => (
+const handleClick = () => openModal("login");
+
+const Protected = ({ component: Component, path, loggedIn, exact }) => (
   <Route
-    {...rest}
-    render={(props) =>
-      loggedIn ? (
-        <Component {...props} />
-      ) : (
-        <Redirect to="/login" />
-      )
-    }
+    path={path}
+    exact={exact}
+    render={(props) => (loggedIn ? <Component {...props} /> : handleClick())}
   />
 );
 
-const mapStateToProps = (state) => ({
-  loggedIn: state.session.isAuthenticated,
-});
+const mapStateToProps = (state) => {
+  return { loggedIn: Boolean(state.session.id) };
+};
 
-export const AuthRoute = withRouter(connect(mapStateToProps)(Auth));
+export const AuthRoute = withRouter(connect(mapStateToProps, null)(Auth));
 
 export const ProtectedRoute = withRouter(connect(mapStateToProps)(Protected));
