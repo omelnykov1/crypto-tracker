52b4b92c437d771ee05d831d1300da6705d3c174
omelnykov1 <sashqa.melnikov@gmail.com>
2020-08-20 14:40:36 -0400

    charts.js

--- frontend/src/components/crypto/ticker/ticker.jsx
+++ frontend/src/components/crypto/ticker/ticker.jsx
@@ -1,11 +1,61 @@
 import React from 'react';
 import { withRouter } from 'react-router';
+import { Bar, Line } from "react-chartjs-2";
 
 
 class Ticker extends React.Component {
     constructor(props) {
         super(props);
         this.handleTable = this.handleTable.bind(this); 
+        this.state = {
+          chartData: {
+            label: ["Bitcoin"],
+            fill: false,
+            lineTension: 0.1,
+            pointBackgroundColor: "#fff",
+            pointBorderWidth: 1,
+            pointHoverRadius: 5,
+            pointHoverBackgroundColor: "rgba(75,192,192,1)",
+            pointHoverBorderColor: "rgba(220,220,220,1)",
+            pointHoverBorderWidth: 2,
+            pointRadius: 1,
+            pointHitRadius: 10,
+            datasets: [
+              {
+                label: "Price",
+                data: [
+                  12284.61728386478,
+                  12310.870864666433,
+                  12297.102212727721,
+                  12301.436996966882,
+                  12360.592250732558,
+                  12263.636675610811,
+                  12323.340938836871,
+                  12302.460235483417,
+                  12336.320402836844,
+                  12292.075264893769,
+                  12228.387765853236,
+                  12209.195698020638,
+                  12252.299425817348,
+                  12238.206741075679,
+                  12240.491027886625,
+                  12207.507713394505,
+                  12264.386714026346,
+                  12206.427208414862,
+                  12235.87491538292,
+                  12113.787998366115,
+                  11934.904652020516,
+                  11930.59860289085,
+                  11968.848747490325,
+                  11984.221832093981,
+                  11981.122331882207,
+                  11994.594757922974,
+                ],
+                backgroundColor: "#5e57df",
+              },
+            ],
+          },
+        };
     }
 
     componentDidMount() {
@@ -33,8 +83,11 @@ class Ticker extends React.Component {
                 <div>
                   <div>
                     <h1>{name}</h1>
-                    <img src={image.thumb} alt=""/>
-                    <div>${this.props.ticker.market_data.current_price.usd}</div>
+                    <img src={image.thumb} alt="" />
+                    <div>
+                      ${this.props.ticker.market_data.current_price.usd}
+                    </div>
+                    < Line data={this.state.chartData} />
                   </div>
                   <div>
                     <p>{this.props.ticker.description.en}</p>
