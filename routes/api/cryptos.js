8c156df1817874c9f67d27aa631ad0846287d574
omelnykov1 <sashqa.melnikov@gmail.com>
2020-08-25 20:31:20 -0400

    fixed routes

--- routes/api/cryptos.js
+++ routes/api/cryptos.js
@@ -5,7 +5,8 @@ const CircularJSON = require('circular-json');
 
 router.get('/', (req, res) => {
   let tick = req.query['0']
-  axios.get(`https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&ids=${tick}&order=market_cap_desc&per_page=100&page=1&sparkline=false`)
+  axios
+    .get(`https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&ids=${tick}&order=market_cap_desc&per_page=100&page=1&sparkline=false`)
     .then(data => {
       res.json(data.data[0])
     })
@@ -14,16 +15,15 @@ router.get('/', (req, res) => {
 
 router.get('/tickers', (req,res) => {
     axios
-      .get(
-        "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false&price_change_percentage=24h"
-      )
+      .get("https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false&price_change_percentage=24h")
       .then((data) => res.send(CircularJSON.stringify(data.data)))
       .catch((err) => console.log(err));
 });
 
 router.get(`/tickers/:tickerId`, (req,res) => {
     const tickerId = req.params.tickerId;
-    axios.get(`https://api.coingecko.com/api/v3/coins/${tickerId}?market_data=true&sparkline=true`)
+    axios
+      .get(`https://api.coingecko.com/api/v3/coins/${tickerId}?market_data=true&sparkline=true`)
       .then((data) => res.send(CircularJSON.stringify(data.data)));
 })
 
