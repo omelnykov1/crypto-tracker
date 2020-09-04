e5ca89ec8775ca70761dc0e204daba2c320475bd
omelnykov1 <sashqa.melnikov@gmail.com>
2020-09-03 23:00:22 -0400

    added pagination

---
+++ frontend/src/components/crypto/pagination.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const Pagination = ({tickersPerPage, totalTickers, paginate}) => {
+    const pageNumbers = [];
+
+    for (let i = 1; i <= Math.ceil(totalTickers / tickersPerPage); i++) {
+        pageNumbers.push(i);
+    }
+    return (
+      <nav>
+        <ul className="pagination">
+          {pageNumbers.map((n) => (
+            <li className="page-item" key={n}>
+              <a onClick={() => paginate(n)} className="page-link">
+                <span className="page-num">{n}</span>
+              </a>
+            </li>
+          ))}
+        </ul>
+      </nav>
+    );
+}
+
+export default Pagination;
--- frontend/src/components/crypto/ticker_index.jsx
+++ frontend/src/components/crypto/ticker_index.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import TickerIndexItem from './ticker_index_item';
 import TableLoader from '../table/table_loader';
+import Pagination from "./pagination";
 
 class TickerIndex extends React.Component {
     constructor(props) {
@@ -9,13 +10,21 @@ class TickerIndex extends React.Component {
           tickers: [],
           toggle: true,
           clicked: false,
+          currentPage: 1,
+          tickersPerPage: 20
         };
         this.sort = this.sort.bind(this);
+        this.handlePageChange = this.handlePageChange.bind(this);
+        this.paginate = this.paginate.bind(this);
     }
 
     componentDidMount() {
       this.props.fetchTickers().then(action => this.setState({tickers: action.tickers}));
       if (this.props.currentUser.id) this.props.fetchTable(this.props.currentUser.id);
+    }
+
+    handlePageChange(pageNumber) {
+      this.setState({currentPage: pageNumber});
     }
 
     sort(input) {
@@ -24,12 +33,19 @@ class TickerIndex extends React.Component {
         ? this.state.tickers.sort((a, b) => b[input] - a[input])
         : this.state.tickers.sort((a, b) => a[input] - b[input]);
       this.setState({tickers});
+    }
+    
+    paginate(n) {
+      this.setState({currentPage: n})
     } 
 
     render() { 
-      const isLoading = this.state.tickers.length ? true : false;
-      if (this.props.tickers.length) {
-        const tickers = this.state.clicked ? this.state.tickers : this.props.tickers;
+      const isLoading = !this.state.tickers.length;
+      if (this.state.tickers.length) {
+        const { currentPage, tickersPerPage, tickers } = this.state;
+        const indexOfLastTicker = currentPage * tickersPerPage;
+        const indexOfFirstTicker = indexOfLastTicker - tickersPerPage;
+        const currentTickers = this.state.tickers.slice(indexOfFirstTicker,indexOfLastTicker);
         const {changeTable, table} = this.props
         return (
           <div className="ticker-index-main">
@@ -42,20 +58,35 @@ class TickerIndex extends React.Component {
                 <h1 className="ticker-coin-index">Coin</h1>
               </div>
               <div className="ticker-index-labels-right">
-                <h1 className="ticker-index-price" onClick={() => this.sort("current_price")}>Price</h1>
-                <h1 className="ticker-index-volume" onClick={() => this.sort("total_volume")}>24h Volume</h1>
-                <h1 className="ticker-index-market-cap" onClick={() => this.sort("market_cap")}>Mkt Cap</h1>
+                <h1
+                  className="ticker-index-price"
+                  onClick={() => this.sort("current_price")}
+                >
+                  Price
+                </h1>
+                <h1
+                  className="ticker-index-volume"
+                  onClick={() => this.sort("total_volume")}
+                >
+                  24h Volume
+                </h1>
+                <h1
+                  className="ticker-index-market-cap"
+                  onClick={() => this.sort("market_cap")}
+                >
+                  Mkt Cap
+                </h1>
                 <h1 className="ticker-index-label-chart">Last 7 days</h1>
               </div>
             </div>
             <div className="tickers-list">
               <ol>
-                {tickers.map((ticker) => (
-                  < TickerIndexItem 
-                    ticker={ticker} 
-                    key={ticker.id} 
-                    changeTable={changeTable} 
-                    table={table} 
+                {currentTickers.map((ticker) => (
+                  <TickerIndexItem
+                    ticker={ticker}
+                    key={ticker.id}
+                    changeTable={changeTable}
+                    table={table}
                     fetchTickers={this.props.fetchTickers}
                     fetchTickerData={this.props.fetchTickerData}
                     user={this.props.currentUser}
@@ -64,15 +95,19 @@ class TickerIndex extends React.Component {
                 ))}
               </ol>
             </div>
+            <div className="pagination">
+              <Pagination
+                tickersPerPage={tickersPerPage}
+                totalTickers={tickers.length}
+                paginate={this.paginate}
+              />
+            </div>
           </div>
         );
       } else {
-        return < TableLoader loading={true}/>
+        return < TableLoader loading={isLoading} />
       }
     }
 }
 
 export default TickerIndex;
-
-
-// https://api.coingecko.com/api/v3/search/trending - TRENDING!!!
