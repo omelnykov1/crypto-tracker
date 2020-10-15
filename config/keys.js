c1a118eb2a723879df039221e7f516fedf5c4ec6
omelnykov1 <sashqa.melnikov@gmail.com>
2020-10-15 14:32:23 -0400

    keys

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,5 @@
-module.exports = {
-  mongoURI: process.env.mongoURI,
-  secretOrKey: process.env.secretOrKey,
-  newsAPIKey: process.env.newsAPIKey,
-};
+if (process.env.NODE_ENV === "production") {
+  module.exports = require("./keys_prod");
+} else {
+  module.exports = require("./keys_dev");
+}
