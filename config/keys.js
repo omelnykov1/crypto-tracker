142bffb08401d00f3c6a2f063b077a4c5606a179
omelnykov1 <sashqa.melnikov@gmail.com>
2020-10-15 14:03:02 -0400

    keys

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,6 @@
-if (process.env.NODE_ENV === 'production') {
-    module.exports = require('./keys_prod');
-} else {
-    module.exports = require('./keys_dev');
-}
+module.exports = {
+  mongoURI:
+    "mongodb+srv://melnykov-dev:wVykFnsxfDqkeWMG@cluster0.rs5df.mongodb.net/CrypdoDB?retryWrites=true&w=majority",
+  secretOrKey: "secret",
+  newsAPIKey: "c5a1cc535fd94b3684073d1475f1d422",
+};
